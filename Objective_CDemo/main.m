cb8bf5a4b6b45ef1565f5816097e096a36581d53
tim_xia <tim_xia@icloud.com>
2018-11-10 09:29:02 +0800

    Initial Commit

---
+++ Objective_CDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective_CDemo
+//
+//  Created by 夏天明 on 2018/11/10.
+//  Copyright © 2018年 夏天明. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
